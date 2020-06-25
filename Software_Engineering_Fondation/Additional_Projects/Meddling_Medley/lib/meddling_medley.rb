48317d1877b674bad37cf1da7d67570b6e2167aa
didishas <didier.ukanda@gmail.com>
2020-06-25 21:25:00 +0200

    Meddling_Medley conjunct_select Challenge

--- Software_Engineering_Fondation/Additional_Projects/Meddling_Medley/lib/meddling_medley.rb
+++ Software_Engineering_Fondation/Additional_Projects/Meddling_Medley/lib/meddling_medley.rb
@@ -170,19 +170,30 @@ end
 # uncompress('a2b4c1') # 'aabbbbc'
 # uncompress('b1o2t1') # 'boot'
 # uncompress('x3y1x2z4') # 'xxxyxxzzzz'
+#
+
 # Phase 2: More difficult, maybe?
 # conjunct_select
 # Write a method conjunct_select that accepts an array and one or more procs as arguments. The method should return a new array containing the elements that return true when passed into all of the given procs.
+
+def conjunct_select(array, *prcs)
+  new_arr = []
+  
+  array.each do |ele|
+    new_arr << ele if prcs.all? {|prc| prc.call(ele)}
+  end
+  new_arr
+end
 # 
 # Examples
 # 
-# is_positive = Proc.new { |n| n > 0 }
-# is_odd = Proc.new { |n| n.odd? }
-# less_than_ten = Proc.new { |n| n < 10 }
-# 
-# p conjunct_select([4, 8, -2, 11, 7, -3, 13], is_positive) # [4, 8, 11, 7, 13]
-# p conjunct_select([4, 8, -2, 11, 7, -3, 13], is_positive, is_odd) # [11, 7, 13]
-# p conjunct_select([4, 8, -2, 11, 7, -3, 13], is_positive, is_odd, less_than_ten) # [7]
+is_positive = Proc.new { |n| n > 0 }
+is_odd = Proc.new { |n| n.odd? }
+less_than_ten = Proc.new { |n| n < 10 }
+
+p conjunct_select([4, 8, -2, 11, 7, -3, 13], is_positive) # [4, 8, 11, 7, 13]
+p conjunct_select([4, 8, -2, 11, 7, -3, 13], is_positive, is_odd) # [11, 7, 13]
+p conjunct_select([4, 8, -2, 11, 7, -3, 13], is_positive, is_odd, less_than_ten) # [7]
 # convert_pig_latin
 # Write a method convert_pig_latin that accepts a sentence as an argument. The method should translate the sentence according to the following rules:
 # 
