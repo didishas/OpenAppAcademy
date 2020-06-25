53631c7be27052d0dfe284b2beeaa591a6e6ca56
didishas <didier.ukanda@gmail.com>
2020-06-25 12:49:03 +0200

    Perilous_procs Picky_Procipher Challenge

--- Software_Engineering_Fondation/Additional_Projects/Perilous_Procs/lib/perilous_procs.rb
+++ Software_Engineering_Fondation/Additional_Projects/Perilous_Procs/lib/perilous_procs.rb
@@ -549,3 +549,106 @@ end
 #     is_yelled => reverse,
 #     contains_a => make_question
 # ) # "STOP:) that??? taxi??? !won"
+
+
+
+
+# picky_procipher
+# Write a method picky_procipher that accepts a sentence and a hash as arguments.
+# The hash contains procs as both keys and values. The method should return 
+# a new sentence where each word of the input sentence is changed by a value 
+# proc if the original word returns true when passed into the key proc.
+# If an original word returns true for multiple key procs, then only the value proc 
+# that appears earliest in the hash should be applied.
+
+
+def picky_procipher(sentence, hash)
+
+	words = sentence.split(" ")
+
+	new_words = []
+	words.each.with_index do |word, index|
+		hash.each do |prc_keys, prc_vals|
+			 if prc_keys.call(words[index])
+				 words[index] = prc_vals.call(words[index])
+				 break
+			 end
+		end
+	end
+	words.join(" ")	
+
+end
+
+# is_yelled = Proc.new { |s| s[-1] == '!' }
+# is_upcase = Proc.new { |s| s.upcase == s }
+# contains_a = Proc.new { |s| s.downcase.include?('a') }
+# make_question = Proc.new { |s| s + '???' }
+# reverse = Proc.new { |s| s.reverse }
+# add_smile = Proc.new { |s| s + ':)' }
+# 
+# p procipher('he said what!',
+#     is_yelled => make_question,
+#     contains_a => reverse
+# ) # "he dias ???!tahw"
+# 
+# p procipher('he said what!',
+#     contains_a => reverse,
+#     is_yelled => make_question
+# ) # "he dias !tahw???"
+# 
+# p procipher('he said what!',
+#     contains_a => reverse,
+#     is_yelled => add_smile
+# ) # "he dias !tahw:)"
+# 
+# p procipher('stop that taxi now',
+#     is_upcase => add_smile,
+#     is_yelled => reverse,
+#     contains_a => make_question
+# ) # "stop that??? taxi??? now"
+# 
+# p procipher('STOP that taxi now!',
+#     is_upcase => add_smile,
+#     is_yelled => reverse,
+#     contains_a => make_question
+# ) # "STOP:) that??? taxi??? !won"
+
+
+# picky_procipher
+# Write a method picky_procipher that accepts a sentence and a hash as arguments. The hash contains procs as both keys and values. The method should return a new sentence where each word of the input sentence is changed by a value proc if the original word returns true when passed into the key proc. If an original word returns true for multiple key procs, then only the value proc that appears earliest in the hash should be applied.
+# 
+# Examples
+
+is_yelled = Proc.new { |s| s[-1] == '!' }
+is_upcase = Proc.new { |s| s.upcase == s }
+contains_a = Proc.new { |s| s.downcase.include?('a') }
+make_question = Proc.new { |s| s + '???' }
+reverse = Proc.new { |s| s.reverse }
+add_smile = Proc.new { |s| s + ':)' }
+
+p picky_procipher('he said what!',
+    is_yelled => make_question,
+    contains_a => reverse
+) # "he dias what!???"
+
+p picky_procipher('he said what!',
+    contains_a => reverse,
+    is_yelled => make_question
+) # "he dias !tahw"
+
+p picky_procipher('he said what!',
+    contains_a => reverse,
+    is_yelled => add_smile
+) # "he dias !tahw"
+
+p picky_procipher('stop that taxi now',
+    is_upcase => add_smile,
+    is_yelled => reverse,
+    contains_a => make_question
+) # "stop that??? taxi??? now"
+
+p picky_procipher('STOP that taxi!',
+    is_upcase => add_smile,
+    is_yelled => reverse,
+    contains_a => make_question
+) # "STOP:) that??? !ixat"
