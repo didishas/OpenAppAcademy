9a2f92496fc441bbadb275ff2ce960ddb2b3c805
didishas <didier.ukanda@gmail.com>
2020-06-25 17:39:11 +0200

    Meddling_Medley counted_characters Challenge

--- Software_Engineering_Fondation/Additional_Projects/Meddling_Medley/lib/meddling_medley.rb
+++ Software_Engineering_Fondation/Additional_Projects/Meddling_Medley/lib/meddling_medley.rb
@@ -78,12 +78,23 @@ end
 # counted_characters
 # Write a method counted_characters that accepts a string as an argument. The method should return an array containing the characters of the string that appeared more than twice. The characters in the output array should appear in the same order they occur in the input string.
 # 
+def counted_characters(string)
+  chars = string.split('')
+  hash = Hash.new(0)
+
+  chars.each do |char|
+    hash[char] += 1
+  end
+
+  hash.select! {|char, count| count > 2}
+  hash.keys
+end
 # Examples
 # 
-# p counted_characters("that's alright folks") # ["t"]
-# p counted_characters("mississippi") # ["i", "s"]
-# p counted_characters("hot potato soup please") # ["o", "t", " ", "p"]
-# p counted_characters("runtime") # []
+p counted_characters("that's alright folks") # ["t"]
+p counted_characters("mississippi") # ["i", "s"]
+p counted_characters("hot potato soup please") # ["o", "t", " ", "p"]
+p counted_characters("runtime") # []
 # triplet_true?
 # Write a method triplet_true? that accepts a string as an argument and returns a boolean indicating whether or not the string contains three of the same character consecutively.
 # 
