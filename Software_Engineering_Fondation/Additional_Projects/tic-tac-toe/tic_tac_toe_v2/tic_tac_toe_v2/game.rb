0a875453e02a053f9885ee17114455cf88627da1
didishas <didier.ukanda@gmail.com>
2020-07-06 18:59:38 +0200

    Software_Engineering_Foundations Additional_Projects tic-tac-toe-v2 init Challenge

---
+++ Software_Engineering_Fondation/Additional_Projects/tic-tac-toe/tic_tac_toe_v2/tic_tac_toe_v2/game.rb
@@ -0,0 +1,43 @@
+#require_relative 'board'
+#require_relative 'human_player'
+$LOAD_PATH << "."
+
+require 'board.rb'
+require 'human_player'
+
+
+class Game
+  def initialize(player_1_mark, player_2_mark)
+    @board = Board.new
+    @player_1 = HumanPlayer.new(player_1_mark)
+    @player_2 = HumanPlayer.new(player_2_mark)
+    @player_1.active = true
+    @current_player = @player_1
+  end
+
+  def switch_turn
+    @player_1.active, @player_2.active = @player_2.active, @player_1.active
+    return @current_player = ( @player_1.active ? @player_1 : @player_2)
+  end
+
+  def play
+    while @board.empty_positions?
+      position = nil
+      is_valid_position = false
+      while !is_valid_position
+        position  = @current_player.get_position #Get position from the current user
+        is_valid_position = true if @board.valid?(position) && @board.empty?(position)
+      end
+        @board.place_mark(position, @current_player.mark)
+        @board.print
+      if @board.win?(@current_player.mark)
+        print "VICTORY"
+        p "Player: #{@current_player} won"
+        return
+      else
+        switch_turn
+      end
+    end
+    p "Draw"
+  end
+end
