f287aa8558f229c2bc648328192c60385ed2dde1
didishas <didier.ukanda@gmail.com>
2020-07-19 09:45:40 +0200

    Additional_Projects Todo_Board TodoBoard v0.0

--- Software_Engineering_Fondation/Additional_Projects/todo_board/todo_board.rb
+++ Software_Engineering_Fondation/Additional_Projects/todo_board/todo_board.rb
@@ -4,8 +4,9 @@ class TodoBoard
 
   # Section Initialisation
   #
-  def initialize(label)
-    @list = List.new(label)    
+  def initialize
+    @board = Hash.new
+    # @list[label] = List.new(label)    
   end
 
   # Section board command
@@ -16,37 +17,49 @@ class TodoBoard
  #   puts "Enter Command in the format 'CMD <option_1> <option_2> separated by space"
     print "\nEnter a command: "
     cmd, *args = gets.chomp.split(' ')
+    list_label, *args_item = args 
+    @list = @board[list_label.to_sym] if !list_label.nil?
 
     case cmd
+      when 'mklist'
+        @board[args[0].to_sym] = List.new(*args) 
+      when 'ls'
+        @board.each do |label, list|
+          p label
+        end
+      when 'showall'
+        @board.each do |label, list|
+          list.print
+        end
       when 'mktodo'
-        @list.add_item(*args)
+        @list.add_item(*args_item)
       when 'rm'
-        args.map!(&:to_i)
-        @list.remove_item(*args)
+        p args_item.map!(&:to_i)
+        @list.remove_item(*args_item)
       when 'purge'
         @list.purge
       when 'up'
-        args.map!(&:to_i)
-        @list.up(*args)
+        args_item.map!(&:to_i)
+        @list.up(*args_item)
       when 'down'
-        args.map!(&:to_i)
-        @list.down(*args)
+        args_item.map!(&:to_i)
+        @list.down(*args_item)
       when 'toggle'
-        args.map!(&:to_i)
-        @list.toggle_item(*args)
+        args_item.map!(&:to_i)
+        @list.toggle_item(*args_item)
       when 'swap'
-        args.map!(&:to_i)
-        @list.swap(*args)
+        args_item.map!(&:to_i)
+        @list.swap(*args_item)
       when 'sort'
         @list.sort_by_date!
       when 'priority'
         @list.print_priority
       when 'print'
-        if args.empty?
+        if args_item.empty?
           @list.print
         else
-          args.map!(&:to_i)
-          @list.print_full_item(*args)
+          args_item.map!(&:to_i)
+          @list.print_full_item(*args_item)
         end
       when 'quit'
         return false
